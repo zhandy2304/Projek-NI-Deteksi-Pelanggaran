6ba9697277aa9df83b456ddb701f4601f93d96e2
zhandy2304 <ahmadzdy230401@gmail.com>
2022-09-16 15:46:55 +0800

    penambahan tabel grafik traffic lalin

--- data_traffic.php
+++ data_traffic.php
@@ -7,7 +7,25 @@
     <link rel="stylesheet" href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/nucleo-svg.css">
     <link rel="stylesheet" href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/soft-ui-dashboard.min.css?v=1.0.2">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
-    <link rel="stylesheet" href="./assets/css/loopple/loopple.css">
+    <!-- <link rel="stylesheet" href="./assets/css/loopple/loopple.css"> -->
+    <style>
+      .chartjs-table, th, td{
+
+        border-collapse : collapse;
+        border: 1px solid #67748E;
+        padding: 10px;
+        margin-left:20px;
+        font-size:12px;
+      }
+
+      .chartjs-thead {
+        font-weight : bold; 
+      }
+
+      .chartjs-body {
+        text-align : center;
+      }
+    </style>
 </head>
 
 <body class="g-sidenav-show">
@@ -72,7 +90,7 @@
                     </p>
                 </div>
                 <div class="card-body p-3">
-                    <div class="chart">
+                    <div class="chart" id="chart1">
                         <div class="chartjs-size-monitor">
                             <div class="chartjs-size-monitor-expand">
                                 <div class=""></div>
@@ -105,7 +123,7 @@
                             </p>
                         </div>
                         <div class="card-body p-3">
-                            <div class="chart">
+                            <div class="chart" id="chart2">
                                 <div class="chartjs-size-monitor">
                                     <div class="chartjs-size-monitor-expand">
                                         <div class=""></div>
@@ -137,7 +155,7 @@
                             </p>
                         </div>
                         <div class="card-body p-3">
-                            <div class="chart">
+                            <div class="chart" id="chart3">
                                 <div class="chartjs-size-monitor">
                                     <div class="chartjs-size-monitor-expand">
                                         <div class=""></div>
@@ -197,6 +215,14 @@
     <script src="https://demos.creative-tim.com/soft-ui-dashboard/assets/js/plugins/chartjs.min.js"></script>
     <script src="https://demos.creative-tim.com/soft-ui-dashboard/assets/js/plugins/Chart.extension.js"></script>
     <script src="https://demos.creative-tim.com/soft-ui-dashboard/assets/js/soft-ui-dashboard.min.js?v=1.0.2"></script>
+    <script>
+
+    function refreshPage(){
+    window.location.reload();
+    } 
+    </script>
+
+
     <?php 
     $conn = new mysqli('localhost', 'root', '', 'jalan_toll');
     $query = $conn->query("
@@ -286,9 +312,7 @@
            	gradientStroke2.addColorStop(1, "rgba(20,23,39,0.2)");
            	gradientStroke2.addColorStop(0.2, "rgba(72,72,176,0.0)");
            	gradientStroke2.addColorStop(0, "rgba(20,23,39,0)");
-           	new Chart(ctx2, {
-              type: "line",
-              data: {
+            const data = {
                 labels: <?php echo json_encode($day) ?>,
                 datasets: [{
                     label: "Mobil",
@@ -326,10 +350,13 @@
                     maxBarThickness: 6
                   },
                 ],
-              },
+              }
+           	new Chart(ctx2, {
+              type: "line",
+              data,
               options: {
                 responsive: true,
-                maintainAspectRatio: false,
+                maintainAspectRatio: true,
                 plugins: {
                   legend: {
                     display: false,
@@ -382,7 +409,47 @@
                   },
                 },
               },
-            }); 
+            });
+            function createTable(){
+              console.log('triggered');
+              const chart = document.querySelector('.chart');
+              const tableDiv = document.createElement('DIV');
+              tableDiv.setAttribute('id', 'tableDiv'); //menambahkan ID
+
+              const table = document.createElement('TABLE');
+              table.classList.add('chartjs-table');
+
+              // add table head (thead)
+              const thead = table.createTHead();
+              thead.classList.add('chartjs-thead');
+
+              thead.insertRow(0);
+              console.log(data.labels)
+              for(let i = 0; i < data.labels.length; i++) {
+                thead.rows[0].insertCell(i).innerText = data.labels[i];
+                
+              };
+              thead.rows[0].insertCell(0).innerText = 'Jam'
+
+              // add table body
+              const tbody = table.createTBody();
+              thead.classList.add('chartjs-tbody');
+              console.log(data.datasets)
+              data.datasets.map((dataset,index) => {
+                tbody.insertRow(index);
+                for(let i = 0; i < data.datasets[0].data.length; i++){
+                  tbody.rows[index].insertCell(i).innerText = dataset.data[i];
+                };
+                tbody.rows[index].insertCell(0).innerText = dataset.label;
+              })
+
+              
+              // append
+              chart.appendChild(tableDiv);
+              tableDiv.appendChild(table);
+              }
+
+              createTable();
            };
 
            if (document.querySelector("#chart-line4")) {
@@ -395,9 +462,7 @@
            	gradientStroke2.addColorStop(1, "rgba(20,23,39,0.2)");
            	gradientStroke2.addColorStop(0.2, "rgba(72,72,176,0.0)");
            	gradientStroke2.addColorStop(0, "rgba(20,23,39,0)");
-           	new Chart(ctx2, {
-              type: "line",
-              data: {
+            const data = {
                 labels: <?php echo json_encode($day_ablam) ?>,
                 datasets: [{
                     label: "Mobil",
@@ -434,10 +499,13 @@
                     maxBarThickness: 6
                   },
                 ],
-              },
+              }
+           	new Chart(ctx2, {
+              type: "line",
+              data ,
               options: {
                 responsive: true,
-                maintainAspectRatio: false,
+                maintainAspectRatio: true,
                 plugins: {
                   legend: {
                     display: false,
@@ -490,10 +558,50 @@
                   },
                 },
               },
-            }); 
+            });
+            function createTable(){
+              console.log('triggered');
+              const chart = document.querySelector('#chart1');
+              const tableDiv = document.createElement('DIV');
+              tableDiv.setAttribute('id', 'tableDiv'); //menambahkan ID
+
+              const table = document.createElement('TABLE');
+              table.classList.add('chartjs-table');
+
+              // add table head (thead)
+              const thead = table.createTHead();
+              thead.classList.add('chartjs-thead');
+
+              thead.insertRow(0);
+              console.log(data.labels)
+              for(let i = 0; i < data.labels.length; i++) {
+                thead.rows[0].insertCell(i).innerText = data.labels[i];
+                
+              };
+              thead.rows[0].insertCell(0).innerText = 'Jam'
+
+              // add table body
+              const tbody = table.createTBody();
+              thead.classList.add('chartjs-tbody');
+              console.log(data.datasets)
+              data.datasets.map((dataset,index) => {
+                tbody.insertRow(index);
+                for(let i = 0; i < data.datasets[0].data.length; i++){
+                  tbody.rows[index].insertCell(i).innerText = dataset.data[i];
+                };
+                tbody.rows[index].insertCell(0).innerText = dataset.label;
+              })
+
+              
+              // append
+              chart.appendChild(tableDiv);
+              tableDiv.appendChild(table);
+              }
+
+              createTable();
            };
 
-           if (document.querySelector("#chart-line1")) {
+            if (document.querySelector("#chart-line1")) {
            	var ctx2 = document.getElementById("chart-line1").getContext("2d");
            	var gradientStroke1 = ctx2.createLinearGradient(0, 230, 0, 50);
            	gradientStroke1.addColorStop(1, "rgba(203,12,159,0.2)");
@@ -548,7 +656,7 @@
               },
               options: {
                 responsive: true,
-                maintainAspectRatio: false,
+                maintainAspectRatio: true,
                 plugins: {
                   legend: {
                     display: false,
@@ -601,7 +709,7 @@
                   },
                 },
               },
-            }); 
+            });
            };
 
            if (document.querySelector("#chart-line2")) {
@@ -658,7 +766,7 @@
               },
               options: {
                 responsive: true,
-                maintainAspectRatio: false,
+                maintainAspectRatio: true,
                 plugins: {
                   legend: {
                     display: false,
@@ -711,8 +819,8 @@
                   },
                 },
               },
-            }); 
+            });
            };
     </script>
-    <script src="./assets/js/loopple/loopple.js"></script>
+    <!-- <script src="./assets/js/loopple/loopple.js"></script> -->
 </body>
